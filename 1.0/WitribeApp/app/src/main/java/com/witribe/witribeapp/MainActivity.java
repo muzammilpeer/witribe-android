1595109a09da739ce5d0beb891ff6f3b784dd7ab
Muzammil Peer <muzammilpeer987@gmail.com>
2015-09-27 17:56:30 +0500

    base

--- 1.0/WitribeApp/app/src/main/java/com/witribe/witribeapp/MainActivity.java
+++ 1.0/WitribeApp/app/src/main/java/com/witribe/witribeapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.witribe.witribeapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.ranisaurus.baselayer.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- 1.0/WitribeApp/app/src/main/java/com/witribe/witribeapp/MainActivityFragment.java
+++ 1.0/WitribeApp/app/src/main/java/com/witribe/witribeapp/MainActivityFragment.java
@@ -1,15 +1,16 @@
 package com.witribe.witribeapp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ranisaurus.baselayer.fragment.BaseFragment;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends BaseFragment {
 
     public MainActivityFragment() {
     }
