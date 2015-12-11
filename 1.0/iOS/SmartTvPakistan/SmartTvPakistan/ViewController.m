ce4fda22846b715b32f5a7824898ca2158042e6a
muzammilpeer <muzammilpeer987@gmail.com>
2015-12-11 17:04:00 +0500

    iOS project added

---
+++ 1.0/iOS/SmartTvPakistan/SmartTvPakistan/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SmartTvPakistan
+//
+//  Created by Muzammil Peer on 12/11/15.
+//  Copyright © 2015 Muzammil Peer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
